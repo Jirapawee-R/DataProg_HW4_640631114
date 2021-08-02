a7deb30041f218664ba5876dba6d21788f3ae55d
Jirapawee-R <42169410+Jirapawee-R@users.noreply.github.com>
2021-08-03 02:49:24 +0700

    function to generate all possible number

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-input_number = input("Enter positive integer to list (separate by space) : ")
+def create_possible_number(input_list):
+    if len(input_list) == 0:
+        return input_list
+    elif len(input_list) == 1:
+        return input_list
+    else:
+        result = []
 
-user_list = input_number.split()
-print('list : ', user_list)
+        # Find all sample space of number that could create from number in list
+        for i in range(len(input_list)):
+            # Define first element in list
+            a = input_list[i]
+            # Create another list to collect remaining element in the first list
+            in_list = input_list[:i] + input_list[i+1:]
+
+            # Generate all possible value that start with element 'a'
+            for j in create_possible_number(in_list):
+                result.append(a + j)
+
+        return result
+
+
+input_number = input("Enter positive integer to list (separate by space) : ").split()
+print('Input list : ', input_number)
+
+sample_space = []
+for p in create_possible_number(input_number):
+    print(p)
+
